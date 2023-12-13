75dfd5e25008f75443403aebf6120ac2ece8d991
GiovanniManfredi <manfredigiovanni64@gmail.com>
2023-12-13 14:44:17 +0100

    14:44

---
+++ Bookster.py
@@ -0,0 +1,11 @@
+import flask
+
+app = flask.Flask(__name__, template_folder='./flask-environment/templates', static_folder='./flask-environment/static')
+
+@app.route('/')
+def home():
+    return flask.render_template('home.html')
+    
+if __name__ == '__main__':
+    app.debug = True
+    app.run(host='0.0.0.0', port=9000, threaded=True, debug=True)
